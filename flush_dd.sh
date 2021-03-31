9beddd406e009da801758eaa11cac418f0c32ba6
Dong Du <ddnirvana1@gmail.com>
2021-03-31 20:43:05 +0800

    dirty updates: many details
    Signed-off-by: Dong Du <ddnirvana1@gmail.com>

---
+++ flush_dd.sh
@@ -0,0 +1,1 @@
+sudo dd if=./Penglai-Opensbi-TVM/build/platform/generic/firmware/fw_payload.bin of=/dev/sdd bs=1M
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -17,6 +17,9 @@ fi
 export host_dir=$PWD/Penglai-sdk-TVM/demo/host
 cp $host_dir/host copy-files/host/
 
+export init_dir=$PWD/Penglai-sdk-TVM/demo/dd-init
+cp $init_dir/dd-init copy-files/host/
+
 #############
 # hello world
 #############
@@ -33,33 +36,33 @@ cp $hello_world_dir/hello-world copy-files/hello-world
 # serverless
 ################
 
-if [ ! -d "copy-files/serverless-test" ] ; then
-  mkdir copy-files/serverless-test
-fi
-
-export serverless_test_dir=$PWD/Penglai-sdk-TVM/demo/image-process/serverless_test
-cp $serverless_test_dir/serverless-user copy-files/serverless-test
-cp $serverless_test_dir/entry_enclave copy-files/serverless-test
-cp $serverless_test_dir/imageResize copy-files/serverless-test
-cp $serverless_test_dir/imageRotate copy-files/serverless-test
-cp $serverless_test_dir/imageErosion copy-files/serverless-test
-
-###############
-# map-reduce
-###############
-
-if [ ! -d "copy-files/map-reduce" ] ; then
-  mkdir copy-files/map-reduce
-fi
-
-export map_reduce_dir=$PWD/Penglai-sdk-TVM/demo/map-reduce
-cp $map_reduce_dir/map/map copy-files/map-reduce
-cp $map_reduce_dir/mphost/mphost copy-files/map-reduce
-cp $map_reduce_dir/mphost/input.txt copy-files/map-reduce
-cp $map_reduce_dir/reduce/reduce copy-files/map-reduce
-cp $map_reduce_dir/map2/map2 copy-files/map-reduce
-cp $map_reduce_dir/mphost2/mphost2 copy-files/map-reduce
-cp $map_reduce_dir/reduce2/reduce2 copy-files/map-reduce
-cp $map_reduce_dir/map4/map4 copy-files/map-reduce
-cp $map_reduce_dir/mphost4/mphost4 copy-files/map-reduce
-cp $map_reduce_dir/reduce4/reduce4 copy-files/map-reduce
+#if [ ! -d "copy-files/serverless-test" ] ; then
+#  mkdir copy-files/serverless-test
+#fi
+#
+#export serverless_test_dir=$PWD/Penglai-sdk-TVM/demo/image-process/serverless_test
+#cp $serverless_test_dir/serverless-user copy-files/serverless-test
+#cp $serverless_test_dir/entry_enclave copy-files/serverless-test
+#cp $serverless_test_dir/imageResize copy-files/serverless-test
+#cp $serverless_test_dir/imageRotate copy-files/serverless-test
+#cp $serverless_test_dir/imageErosion copy-files/serverless-test
+#
+################
+## map-reduce
+################
+#
+#if [ ! -d "copy-files/map-reduce" ] ; then
+#  mkdir copy-files/map-reduce
+#fi
+#
+#export map_reduce_dir=$PWD/Penglai-sdk-TVM/demo/map-reduce
+#cp $map_reduce_dir/map/map copy-files/map-reduce
+#cp $map_reduce_dir/mphost/mphost copy-files/map-reduce
+#cp $map_reduce_dir/mphost/input.txt copy-files/map-reduce
+#cp $map_reduce_dir/reduce/reduce copy-files/map-reduce
+#cp $map_reduce_dir/map2/map2 copy-files/map-reduce
+#cp $map_reduce_dir/mphost2/mphost2 copy-files/map-reduce
+#cp $map_reduce_dir/reduce2/reduce2 copy-files/map-reduce
+#cp $map_reduce_dir/map4/map4 copy-files/map-reduce
+#cp $map_reduce_dir/mphost4/mphost4 copy-files/map-reduce
+#cp $map_reduce_dir/reduce4/reduce4 copy-files/map-reduce

2cfe6ba61247093961f5a7713374953e9869d349
fengeh <2748250768@qq.com>
2021-04-11 10:15:04 +0800

    add test: redis

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -54,17 +54,17 @@ cp $ipc_dir/test-caller/test-caller-baseline copy-files/ipc
 # serverless
 ################
 
-#if [ ! -d "copy-files/serverless-test" ] ; then
-#  mkdir copy-files/serverless-test
-#fi
-#
-#export serverless_test_dir=$PWD/Penglai-sdk-TVM/demo/image-process/serverless_test
-#cp $serverless_test_dir/serverless-user copy-files/serverless-test
-#cp $serverless_test_dir/entry_enclave copy-files/serverless-test
-#cp $serverless_test_dir/imageResize copy-files/serverless-test
-#cp $serverless_test_dir/imageRotate copy-files/serverless-test
-#cp $serverless_test_dir/imageErosion copy-files/serverless-test
-#
+if [ ! -d "copy-files/serverless-test" ] ; then
+ mkdir copy-files/serverless-test
+fi
+
+export serverless_test_dir=$PWD/Penglai-sdk-TVM/demo/image-process/serverless_test
+cp $serverless_test_dir/serverless-user copy-files/serverless-test
+cp $serverless_test_dir/entry_enclave copy-files/serverless-test
+cp $serverless_test_dir/imageResize copy-files/serverless-test
+cp $serverless_test_dir/imageRotate copy-files/serverless-test
+cp $serverless_test_dir/imageErosion copy-files/serverless-test
+
 ###############
 # map-reduce
 ###############
@@ -97,3 +97,41 @@ export fs_dir=$PWD/Penglai-sdk-TVM/demo/FS
 cp $fs_dir/fshost/fshost copy-files/fs
 cp $fs_dir/testfs/testfs copy-files/fs
 cp $fs_dir/littlefs/lfs copy-files/fs
+
+#############
+# stress-ng
+#############
+
+if [ ! -d "copy-files/stress-ng" ]; then
+  mkdir copy-files/stress-ng
+fi
+
+export stress_ng_dir=$PWD/Penglai-sdk-TVM/demo/stress-ng
+cp $stress_ng_dir/stress-ng copy-files/stress-ng/
+
+
+############
+# lmbench
+############
+
+if [ ! -d "copy-files/lmbench" ] ; then
+  mkdir copy-files/lmbench
+fi
+
+export lmbench_dir=$PWD/Penglai-sdk-TVM/demo/lmbench
+cp $lmbench_dir/bin/x86_64-linux-gnu/*mem* copy-files/lmbench
+cp $lmbench_dir/bin/x86_64-linux-gnu/*mmap* copy-files/lmbench
+
+############
+# redis
+############
+
+if [ ! -d "copy-files/redis" ] ; then
+  mkdir copy-files/redis
+fi
+
+export redis_dir=$PWD/Penglai-sdk-TVM/demo/redis-6.2.1/src
+cp $redis_dir/redis-benchmark copy-files/redis
+cp $redis_dir/redis-server copy-files/redis
+cp $redis_dir/redis-cli copy-files/redis
+# cp -r $redis_dir/../ copy-files/redis
